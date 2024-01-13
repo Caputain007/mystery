1e97463baa07781bd425a29fbdd144d54fd7b5c1
Dylan Bullock <dylan@BigMac.local>
2024-01-13 10:56:16 -0600

    Updated with naming conventions

--- mystery.py
+++ mystery.py
@@ -1,30 +1,33 @@
 import re
 
-def my_func(x):
-    a = x.split(" ")
-    y = []
-    for x in a:
-        x2 = re.sub("[.!?]", "", x)
-        y.append(x2)
-    z = {}
-    for stuff in y:
-        if len(stuff) in z: 
-            z[len(stuff)] = z[len(stuff)] + [stuff]
+def word_Sort(wordString):
+    word = wordString.split(" ")
+    wordArray = []
+    for wordString in word:
+        #Strip special chars.
+        wordStringUpdated = re.sub("[.!?]", "", wordString)
+        wordArray.append(wordStringUpdated)
+    wordDictionary = {}
+    #Dict wordDictionary based on word len
+    for index in wordArray:
+        if len(index) in wordDictionary: 
+            wordDictionary[len(index)] = wordDictionary[len(index)] + [index]
         else:
-            z[len(stuff)]=[stuff]
-    a = {}
-    for y in z:
-        for x in z[y]:
-            if len(x) % 2 == 1:
-                a[y] = z[y]
+            wordDictionary[len(index)]=[index]
+    wordDictionaryOdd = {}
+    #Dict a w/ modulo operator is checking if odd from wordDictionary dictionary.
+    for index in wordDictionary:
+        for wordString in wordDictionary[index]:
+            if len(wordString) % 2 == 1:
+                wordDictionaryOdd[index] = wordDictionary[index]
         else:
             continue
-    return a
+    return wordDictionaryOdd
 
 
 
 
 # Tests
-assert(my_func("This is a sentence. And yet another one!") == {1: ['a'], 3: ['And', 'yet', 'one'], 7: ['another']})
-assert(my_func("Miscollated alphabetic superimposition") == {11: ['Miscollated'], 15: ['superimposition']})
-print(my_func("a a a a bb bb bb ccc ccc") == {1: ['a', 'a', 'a', 'a'], 3: ['ccc', 'ccc']})
+assert(word_Sort("This is a sentence. And yet another one!") == {1: ['a'], 3: ['And', 'yet', 'one'], 7: ['another']})
+assert(word_Sort("Miscollated alphabetic superimposition") == {11: ['Miscollated'], 15: ['superimposition']})
+assert(word_Sort("a a a a bb bb bb ccc ccc") == {1: ['a', 'a', 'a', 'a'], 3: ['ccc', 'ccc']})
